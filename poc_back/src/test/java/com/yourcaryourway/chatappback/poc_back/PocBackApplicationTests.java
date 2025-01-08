183005aa9f6c3bd546bc592c7c55c34c6a2f3e53
Nobound <jean.ungerer.nobound@gmail.com>
2025-01-08 10:25:40 +0100

    Init back

---
+++ poc_back/src/test/java/com/yourcaryourway/chatappback/poc_back/PocBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourcaryourway.chatappback.poc_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
